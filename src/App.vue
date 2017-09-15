2e31629fe44eae2ecf6227525c0bd16c972d4112
周麒麟 <mrkylin.zhou@gmail.com>
2017-09-15 17:23:02 +0800

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
+        <head-com></head-com>
         <p>Only Start</p>
     </div>
 </template>
 
 <script>
+import headCom from './view/header/header';
 export default {
-    name: 'app'
+    name: 'app',
+    components: {
+        headCom
+    }
 }
 </script>
+
+<style>
+    @import './common/reset.css';
+</style>
---
+++ src/view/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="headband"></div>
+    </div>
+</template>
+
+<style>
+    .headband {
+        background-color: #222;
+        height: 3px;
+    }
+</style>
