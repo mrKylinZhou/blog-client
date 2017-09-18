b6d206b1430638a740b1c76ec3046b8ccf6c9785
周麒麟 <mrkylin.zhou@gmail.com>
2017-09-18 23:50:37 +0800

    back-to-top

---
+++ src/view/back-to-top.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="back-to-top">
+        <i></i>
+    </div>
+</template>
+
+<style lang="less" scoped>
+    .back-to-top {
+        box-sizing: border-box;
+        position: fixed;
+        bottom: -100px;
+        right: 30px;
+        z-index: 1050;
+        padding: 0 6px;
+        width: 24px;
+        background: #222;
+        font-size: 12px;
+        opacity: 1;
+        color: #fff;
+        cursor: pointer;
+        text-align: center;
+        -webkit-transform: translateZ(0);
+        transition-property: bottom;
+        transition-duration: 0.2s;
+        transition-timing-function: ease-in-out;
+        transition-delay: 0s;
+    }
+</style>
