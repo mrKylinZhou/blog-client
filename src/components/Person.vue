dce7ae82528220decc0e175d7aa9091c0ca8c47d
Kylin <mrkylin.zhou@gmail.com>
2018-07-19 14:45:30 +0800

    icon

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -11,5 +11,17 @@
     <mu-card-media>
       <img src="../assets/img/bg.png">
     </mu-card-media>
+    <mu-card-actions>
+      <mu-button
+        icon
+        href="https://github.com/mrKylinZhou">
+        <i class="iconfont icon-github"></i>
+      </mu-button>
+      <mu-button
+        icon
+        href="https://www.zhihu.com/people/kylin-zhou/activities">
+        <i class="iconfont icon-zhihu"></i>
+      </mu-button>
+    </mu-card-actions>
   </mu-card>
 </template>
